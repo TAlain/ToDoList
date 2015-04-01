7a85d3cdf60a871a69f76440f53f2c2ff45e5f92
alaint <alain_thoen@hotmail.com>
2015-04-01 14:04:18 +0200

    Init

---
+++ ToDoList/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace ToDoList.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using ToDoList.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ToDoList.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "ToDoList.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(ToDoList.Models.ApplicationDbContext context)
+        {
+            if (!context.Users.Any(u => u.Email == "Alain_thoen@hotmail.com"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { Email = "Alain_thoen@hotmail.com", UserName = "Alain" };
+
+                manager.Create(user, "password");
+
+            }
+        }
+    }
+}
---
+++ ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Workstate Workstate  { get; set; }
+        public virtual ApplicationUser AssignedUser { get; set; }
+    }
+    public enum Workstate
+    {
+        [Display(Name="To Do")]
+        Todo,
+        [Display(Name="In Progress")]
+        InProgress,
+        Done
+    }
+}
